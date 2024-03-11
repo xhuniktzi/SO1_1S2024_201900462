2c6ecfe7ae68dfb69afdeb1c51358aa98f37b32e
Xhunik Miguel <xhuniktzi@gmail.com>
2024-03-10 22:19:45 -0600

    tree

---
+++ Proyecto1/monitor-frontend/src/TreeProcess.jsx
@@ -0,0 +1,80 @@
+import { graphviz } from 'd3-graphviz'
+import { React, useEffect, useState } from 'react'
+
+const recursiveGenerateDot = (data) => {
+    if (!data) {
+        return ''
+    }
+
+    let dot = `node${data.PID} [label="{PID: ${data.PID} | Name: ${data.Name} }"];\n`
+
+    if (data.Children) {
+        data.Children.forEach(child => {
+            dot += `node${data.PID} -> node${child.PID};\n`
+            dot += recursiveGenerateDot(child)
+        })
+    }
+
+    return dot
+}
+
+const recursiveFindPidLayer1 = (pid, data) => {
+    if (!data) {
+        return
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].PID == pid) {
+
+            return data[i]
+        } else {
+            recursiveFindPidLayer0(pid, data[i].Children)
+        }
+    }
+}
+const recursiveFindPidLayer0 = (pid, data) => {
+
+    if (!data) {
+        return
+    }
+
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].PID == pid) {
+
+            return data[i]
+        } else {
+            recursiveFindPidLayer1(pid, data[i].Children)
+        }
+    }
+}
+
+const TreeProcess = () => {
+    const [data, setData] = useState([])
+    const [dot, setDot] = useState('')
+
+    const fetchData = async () => {
+        const response = await fetch('http://localhost:8080/cpu')
+        setData(await response.json())
+
+        const pid = document.getElementById('pid').value
+
+        const result = recursiveFindPidLayer0(pid, data.Processes)
+        setDot(recursiveGenerateDot(result))
+    }
+
+    useEffect(() => {
+        graphviz("#graph")
+            .renderDot(`digraph { node [shape=record];\n ${dot} \n}`)
+    }, [dot])
+
+    return (
+        <>
+            <label htmlFor="pid">PID: </label>
+            <input type="number" id="pid" name="pid" />
+            <button type="button" onClick={fetchData}>Buscar</button>
+            <div id="graph" />
+        </>
+    )
+}
+
+export default TreeProcess;
