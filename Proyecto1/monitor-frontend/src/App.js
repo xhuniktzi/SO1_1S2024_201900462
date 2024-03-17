1bfce644412fe46e771dd8a7b34b60be0c084d49
Xhunik Miguel <xhuniktzi@gmail.com>
2024-03-16 18:13:24 -0600

    simulador

--- Proyecto1/monitor-frontend/src/App.js
+++ Proyecto1/monitor-frontend/src/App.js
@@ -4,24 +4,29 @@ import TreeProcess from './TreeProcess';
 import RamUsage from './RamUsage';
 import CpuUsage from './CpuUsage';
 import ChartComponent from './Historic';
+import ProcessSimulator from './SimulatorProc';
 
 function App() {
   return (
     <>
+      <ProcessSimulator />
       <TreeProcess />
       <ChartComponent />
+
 
       <div className="wrapper">
         <div className="chartsjs">
           <h1>CPU</h1>
           <CpuUsage />
         </div>
-        
+
         <div className="chartsjs">
           <h1>RAM</h1>
           <RamUsage />
         </div>
       </div>
+
+
     </>
   );
 }
